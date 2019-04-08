443e09ac3534a21fcea992a7cc9af229dc39f8c8
Javier Calin de Briones Garcia <Javier.Calin@sizmek.com>
2019-04-08 09:25:34 +0200

    add morgan, body parsera and nodemon. adding headers and handling errors

--- api/rutes/products.js
+++ api/rutes/products.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 router.get('/', (req, res,next)=>{
     res.status(200).json({
-        message: 'Handling GET request to /products ;)'
+        message: 'Handling GET request to /products'
     })
 })
 router.post('/', (req, res,next)=>{
+    const product ={
+        name: req.body.name,
+        price: req.body.price
+    }
     res.status(201).json({
-        message: 'Handling POST request to /products'
+        message: 'Handling POST request to /products',
+        createdProduct: product
     })
 })
 router.get('/:productId', (req, res, next)=>{
