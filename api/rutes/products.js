0ed7f34d483a2bfe74ffcebec6ceafdc98eb62f8
Javi <creativesupport-spain@sizmek.com>
2019-04-15 13:44:53 +0200

    Added post, get, delete, patch methods with errors

--- api/rutes/products.js
+++ api/rutes/products.js
@@ -4,8 +4,17 @@ const mongoose = require('mongoose')
 const Product = require('../models/product')
 
 router.get('/', (req, res,next)=>{
-    res.status(200).json({
-        message: 'Handling GET request to /products'
+    Product.find()
+    .exec()
+    .then(docs=>{
+        console.log(docs)
+        res.status(200).json(docs)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error : err
+        })
     })
 })
 router.post('/', (req, res,next)=>{
@@ -17,37 +26,76 @@ router.post('/', (req, res,next)=>{
 
     product.save().then(result=> {
             console.log(result)
-        }).catch(err=> console.log(err))
+            res.status(201).json({
+                message: 'Handling POST request to /products',
+                createdProduct: result
+            })
+        }).catch(err=> {
+            console.log(err)
+            res.status(500).json({
+                error:err
+            })
+        })
 
-    res.status(201).json({
-        message: 'Handling POST request to /products',
-        createdProduct: product
-    })
+    
 })
 router.get('/:productId', (req, res, next)=>{
     const id = req.params.productId;
-    if(id==='special'){
-        res.status(200).json({
-            message: 'you discover special id',
-            id : id
+    Product.findById(id)
+        .exec()
+        .then(doc=>{
+            console.log("from database", doc)
+            if (doc) {
+              res.status(200).json(doc)  
+            }else{
+                res.status(400).json({
+                    message: "No valid entry foun for provided ID"
+                })
+            }
+            
         })
-    }else{
-        res.status(200).json({
-            message: 'you pass an id'
+        .catch(err=>{
+            console.log(err)
+            res.status(500).json({
+                error:err
+            })
         })
-    }
+
 })
 
 router.patch('/:productId', (req, res, next)=>{
-   res.status(200).json({
-       message: 'Updated product'
-   })
+    const id= req.params.productId;
+    const updateOps = {}
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value
+    }
+    Product.update({_id:id},updateOps)
+        .exec()
+        .then(result=>{
+            console.log(result)
+            res.status(200).json(result)
+        })
+        .catch(err=>{
+            console.log(err)
+            res.status(500).json({
+                error:err
+            })
+        })
 })
 
 router.delete('/:productId', (req, res, next)=>{
-    res.status(200).json({
-        message: 'Deleted product'
-    })
+    const id= req.params.productId;
+    Product.remove({_id:id})
+        .exec()
+        .then(result =>{
+            res.status(200).json(result)
+        })
+        .catch(err=>{
+            console.log(err)
+            res.json(500).json({
+                error:err
+            })
+        })
  })
 
 module.exports = router;
